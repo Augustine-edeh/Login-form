add5cffc404c9ff4d9f618946bc9a5f629786537
Augustine Edeh <info.augustinesedeh@gmail.com>
2021-06-16 10:02:10 +0100

    Create Main.js

---
+++ Main.js
@@ -0,0 +1,14 @@
+function bder() {
+  var e = document.getElementById("n");
+  e.setAttribute("style", "border: none;");
+//alert("hello")
+}
+
+
+
+
+ function sv() {
+   a ="'"
+   var e = document.getElementById("_n").value;
+   alert("Your password is " + a + e + a);
+ }
